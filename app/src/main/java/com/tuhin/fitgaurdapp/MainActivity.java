0c92d0fd3eee70f7aa769928fe53fe7a9b2c871f
tam1mBD <st.tamim.2001@gmail.com>
2023-12-22 14:55:28 +0600

    adding bottom navigation and three fragment

--- app/src/main/java/com/tuhin/fitgaurdapp/MainActivity.java
+++ app/src/main/java/com/tuhin/fitgaurdapp/MainActivity.java
@@ -1,14 +1,62 @@
 package com.tuhin.fitgaurdapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.tuhin.fitgaurdapp.R;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewPager2 viewPager2;
+    ViewPagerAdapter viewPagerAdapter;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        viewPager2 =findViewById(R.id.viewPager);
+        viewPagerAdapter= new ViewPagerAdapter(this);
+        viewPager2.setAdapter(viewPagerAdapter);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.bottom_home) {
+                    viewPager2.setCurrentItem(0);
+                } else if (id == R.id.bottom_alarm) {
+                    viewPager2.setCurrentItem(1);
+                } else if (id == R.id.bottom_profile) {
+                    viewPager2.setCurrentItem(2);
+                }
+                return false;
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.bottom_home).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.bottom_alarm).setChecked(true);
+                        break;
+                    case 2:
+                        bottomNavigationView.getMenu().findItem(R.id.bottom_profile).setChecked(true);
+                        break;
+                }
+                super.onPageSelected(position);
+            }
+        });
     }
 }

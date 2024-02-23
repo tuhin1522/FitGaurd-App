33b798b3b1aa3e7f02f8a0f5109275e2f8da344e
MD. TUHIN MOLLA <mdtuhincse15jkkniu@gmail.com>
2024-02-24 02:38:11 +0600

    Updated Design

--- app/src/main/java/com/tuhin/fitgaurdapp/Profile.java
+++ app/src/main/java/com/tuhin/fitgaurdapp/Profile.java
@@ -10,12 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 public class Profile extends Fragment {
     TextView name, dateOfBirth, gender, weight, height;
-    Button btnLogout;
+    LinearLayout btnLogout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
